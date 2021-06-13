abff1443d2283e88b294126bc5d096b23ef6aded
moynul457 <69568834+moynul457@users.noreply.github.com>
2021-06-13 22:16:51 +0600

    Add files via upload

---
+++ SA.sh
@@ -0,0 +1,275 @@
+#!/bin/bash
+#APAANLUANJINGMAURECODEYAKONTOLMODALDONG ASU BIKIN AJA SENDIRI APA SUSAHNYA
+#BIKIN CUMAN MODAL ECHO IF FI APA SUSAH NYA KONTOL RECODE MULU HIDUP LU
+#BAU MEMECK  LU YA BGSD
+
+red='\e[1;31m'
+green='\e[1;32m'
+yellow='\e[1;33m'
+blue='\e[1;34m'
+magenta='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+#gantikodewarna
+k="\033[33;1m"
+m="\033[31;1m"
+pu="\033[39;1m"
+p="\033[36;1m"
+b="\033[34;1m"
+h="\033[32;1m"
+JAM=`date +%H`
+MENIT=`date +%M`
+DETIK=`date +%S`
+TANGGAL=`date +%d`
+BULAN=`date +%m`
+TAHUN=`date +%Y`
+mp="\033[41;1;39m"
+o="\033[0m"
+clear
+echo
+echo "         ${m}[ ${pu}TIME ZONE PAKISTAN ${m}]"
+echo "${b}   ╔════════╗${b} ╔═══════🕜═╗${b} ╔═══════🕛═╗"
+echo "${b}   ║${k}JAM:"${m}[${pu}$JAM${m}]${b}║~${b}║${k}MINUTE:"${m}[${pu}$MINUTE${m}]${b}║${b}~║${k}SECONDS:${m}["${pu}$SECONDS${m}]${b}║
+echo "${b}   ╚════════╝${b} ╚══════════╝${b} ╚══════════╝"
+echo "               ${m}[ ${pu}TECHNICAL SAA ${m}]"
+echo "${b}╔════════════╗${b} ╔══════════╗${b} ╔════════════╗"
+echo "${b}║${k}DATE:"${m}[${pu}$DATE${m}]${b}║${b}~║${k}MONTH:"${m}[${pu}$MONTH${m}]${b}║${b}~║${k}YEARS:${m}["${pu}$YEARS${m}]${b}║
+echo "${b}╚════════════╝${b} ╚══════════╝${b} ╚════════════╝"
+echo "${k}<════════════════════════════════════════>"
+echo
+
+
+limit=100
+sleep 1
+echo "\033[031m     ▇▇▇▇▇▇▇"
+echo "\033[037m     ▇▇▇▇▇▇▇"
+echo "\033[031mWelcome \033[037mto Tools SAA"
+sleep 1
+echo -n "Name:"
+read name;
+sleep 2
+echo "Hello, nice to meet you, sir \033[036m[$nama]"
+sleep 1
+echo "\033[037mDon't forget to subscribe to admin channel Technical SAA:)"
+sleep 1
+clear
+echo -n "Have you subscribed to the Technical SAA channel? y/n :"
+read pilih
+if [ $pilih = "y" ];
+then
+sleep 2
+termux-open-url "https://www.youtube.com/channel/UCEmNhaHoLSbxmthbrg4-OYA"
+echo "Thank you for subscribing to the Teacher SAA channel"
+sleep 2
+clear
+elif [ $pilih = "n" ];
+then
+sleep 2
+echo "PLEASE SUBSCRIBE CHANNEL ADMIN"
+termux-open-url "https://www.youtube.com/channel/UCEmNhaHoLSbxmthbrg4-OYA"
+sleep 4
+echo "MAKASIH SUDAH DUKUNG  CHANNEL ADMIN^_^"
+sleep 2
+clear
+else
+echo "you entered the wrong input!!!!"
+sleep 2
+sh hek.sh
+fi
+clear
+sleep 1
+python2 loding692
+sleep 1
+clear
+echo "\033[037m  <═════════════════════════════════════════>"
+echo "  <\>\033[032m WELCOME TO MY TOOLS\033[037m|\033[033mTHE TECHNICA SAA   \033[037m<\>"
+echo "  <═════════════════════════════════════════>"
+sleep 1
+echo "\033[032m   ╲    ╱   \033[031m ●\033[033m T O O L S\033[031m●\033[033m I N S T A L L E R\033[031m●"
+echo "\033[032m   ╱▔▔▔▔╲   \033[036m    Author\033[032m   :\033[037m TECHNICAL SAA"
+echo "\033[032m  ┃┈\033[037m▇\033[032m┈┈\033[037m▇\033[032m┈┃ \033[036m     Youtube\033[032m  :\033[037m TECHNICAL SAA"
+echo "\033[032m╭╮┣━━━━━━┫╭╮\033[036m    Instagram\033[032m:\033[037m TECHNICAL SAA"
+echo "\033[032m┃┃┃┈┈┈┈┈┈┃┃┃\033[036m    Github\033[32m   :\033[037m Technical-SAA.com"
+echo "\033[032m╰╯┃┈┈┈┈┈┈┃╰╯" 
+echo "\033[032m  ╰┓┏━━┓┏╯"   
+echo "\033[032m   ╰╯  ╰╯"                               
+echo "${b} ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${k}ஜ۩��۩ஜ${b}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo "${m} [ ${p}Subscribe Youtube${m}:${p}Technical SAA Channel ${m}]"
+echo "${m}          [ ${h}Version${m}:${c}New ${p}*_* ${m}]"
+echo "${b} ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo "\033[032m[\033[037m+\033[032m]\033[034m===================================================\033[032m[\033[037m+\033[032m]"
+sleep 2
+echo "\033[036m╔═════════════════════════════════════════════╗"
+echo " \033[031m●\033[037mNO\033[031m        \033[031m●\033[037mMENU \033[034mTOOLS\033[031m●             \033[031m●\033[037mSTATUS\033[031m●"
+echo "\033[036m╚═════════════════════════════════════════════╝"
+echo
+echo  "\033[032m[\033[037m1\033[032m]\033[037m.\033[031m [\033[037m HACKING FACEBOOK TERBARU\033[031m   ]    \033[033m[\033[032mON\033[033m]\033[031m●"
+echo
+echo  "\033[032m[\033[037m2\033[032m]\033[037m.\033[031m [\033[037m HACKING INSTAGRAM ACCOUNT\033[031m  ]    \033[033m[\033[032mON\033[033m]\033[031m●"
+echo
+echo  "\033[032m[\033[037m3\033[032m]\033[037m.\033[031m [\033[037m TOOLSXC APMZ CHANNEL\33[031m       ]    \033[033m[\033[032mON\033[033m]\033[031m●"
+echo
+echo  "\033[032m[\033[037m4\033[032m]\033[037m.\033[031m [\033[037m SET TOOLS SPAM SMS      \033[031m   ]    \033[033m[\033[032mON\033[033m]\033[031m●"
+echo
+echo  "\033[032m[\033[037m5\033[032m]\033[037m.\033[031m [\033[037m SPAM WHATSAPP\033[031m              ]    \033[033m[\033[032mON\033[033m]\033[031m●"
+echo
+echo  "\033[032m[\033[037m6\033[032m]\033[037m.\033[031m [\033[037m VIRUS WHATSAPP             \033[031m]    \033[033m[\033[032mON\033[033m]\033[031m●"
+echo
+echo  "\033[032m[\033[037m7\033[032m]\033[037m.\033[031m [\033[037m DARK FB V9\033[031m \033[031m                ]    \033[033m[\033[032mON\033[033m]\033[031m●"
+echo
+echo  "\033[032m[\033[037m8\033[032m]\033[037m.\033[031m [\033[037m HACKING FB AUTOMBF        \033[031m ]    \033[033m[\033[032mON\033[033m]\033[031m●"
+echo
+echo  "\033[032m[\033[037m9\033[032m]\033[037m.\033[031m [\033[037m SPAM TELPHONE              \033[031m]    \033[033m[\033[032mON\033[033m]\033[031m●"
+echo 
+echo  "\033[032m[\033[037m10\033[032m]\033[037m.\033[031m[\033[037m BUAT SCRIPT DEFACE         \033[031m]    \033[033m[\033[032mON\033[033m]\033[031m●"
+echo
+echo  "\033[032m[\033[037m00\033[032m]\033[037m.\033[031m[\033[037m SAA TOOLS                  \033[031m]"
+echo
+echo  "\033[032m[\033[037m99\033[032m]\033[037m.\033[031m[\033[037m SUBSCRIBE CHANNEL T- SAA   \033[031m]    \033[033m[\033[032mWAJIB:V\033[033m]"
+echo "\033[033m|"
+echo "v"
+read -p  "[?]pilih--->:" pilih;
+if [ $pilih = "1" ];
+then
+echo "\033[032m[\033[037m*\033[032m]\033[037mMulai Penginstallan..."
+sleep 2
+apt update && apt upgrade
+apt install python python2
+apt install git
+apt install php
+apt ìnstall nano
+apt install bash
+apt install figlet
+apt install toilet
+apt install ruby
+apt install neofetch
+gem install lolcat
+pip2 install mechanize
+pip2 install requests
+cd tool
+python2 MhwiwowBhwow9OF
+
+elif [ $pilih = "2" ];
+then
+echo "\033[032m[\033[037m*\033[032m]\033[037mMulai Penginstallan..."
+sleep 2
+apt update && apt upgrade
+apt install python2
+apt install git
+apt install python
+apt install curl
+apt install bash
+apt install nano
+apt install nodejs
+pip2 install mechanize
+pip2 install requests
+pip2 install bs4
+pip2 install igtools
+apt install jq -y
+git clone https://github.com/arief125/igtools
+cd igtools
+bash ig.sh
+sleep 2
+
+echo "\033[032m[\033[037mTHANKS UDAH PAKAI TOOLS SAYA ^_^\033[032m]"
+sleep 2
+
+elif [ $pilih = "3" ];
+then
+sleep 1
+echo "\033[032m[\033[037m*\033[032m]\033[037mMulaii Penginstallan.."
+sleep 2
+apt update && apt upgrade
+apt install git
+git clone https://github.com/BangDanz/toolsxc
+cd toolsxc
+sh tool.sh
+sleep 2
+
+elif [ $pilih = "4" ];
+then
+sleep 1
+echo "\033[032m[\033[037m*\033[032m]\033[037mMulai penginstallan..."
+sleep 2
+apt update && apt upgrade
+git clone https://github.com/BangDanz/SpamApmz
+cd SpamApmz
+python bj.py
+sleep 1
+clear
+elif [ $pilih = "5" ];
+then
+sleep 1
+echo "\033[032m[\033[037m*\033[032m]\033[037mMulai Penginstallan..."
+echo "\033[036m"
+sleep 2
+apt update && apt upgrade
+git clone https://github.com/BangDanz/bomwa
+cd bomwa
+python2 Wa.py
+sleep 1
+clear
+elif [ $pilih = "6" ];
+then
+sleep 1
+cd tool
+sh virus.sh
+sleep 1
+elif [ $pilih = "7" ];
+then
+sleep 1
+cd tool
+git clone https://gitlab.com/Sumarr-ID/Darkfbv9
+cd Darkfbv9
+python2 dark.py
+sleep 1
+elif [ $pilih = "8" ];
+then
+clear
+sleep 1
+cd tool
+git clone https://gitlab.com/Sumarr-ID/Autombf
+cd Autombf
+python2 autombf.py
+sleep 1
+elif [ $pilih = "9" ];
+then
+sleep 1
+cd tool
+sh tlp.sh
+sleep 1
+clear
+elif [ $pilih = "10" ];
+then
+sleep 1
+cd tool
+python2 LITESCRIPT.py
+sleep 2
+clear
+elif [ $pilih = "00" ];
+then
+sleep 1
+echo "\033[032m[\033[037m*\033[032m]\033[036mTHANKS SUDAH PAKAI TOOLS SAYA ^_^ "
+sleep 1
+echo "DON'T LOSE SUBSCRIBE CHANNEL TECHNICAL SAA^_^"
+sleep 1
+echo "YAH HANYA INI YANG BISA SAYA BUAT:("
+sleep 1
+echo "SEMOGA BERMANFAAT BAGI PARA USER TERMUX:)"
+exit
+
+elif [ $pilih = "99" ];
+then
+sleep 1
+echo "\033[032m Sedang membuka youtube.."
+sleep 2
+termux-open-url "https://www.youtube.com/channel/UCEmNhaHoLSbxmthbrg4-OYA"
+echo "THANKS SUDAH SUPPORT CHANNEL SAYA^_^"
+sleep 1
+sh hek.sh
+
+else
+echo "input yang anda masukan tidak ada!!"
+sleep 2
+sh hek.sh
+fi
